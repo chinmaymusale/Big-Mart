5095851aca47763cfc541ab6a513dd2c597ee07a
chinmaymusale <40321889+chinmaymusale@users.noreply.github.com>
2019-12-26 16:38:38 +0530

    Add files via upload

--- R code.R
+++ R code.R
@@ -9,7 +9,6 @@ install.packages("caret")
 install.packages("corrplot")
 install.packages("xgboost")
 install.packages("cowplot")
-
 library(data.table)
 library(dplyr)  
 library(ggplot2)  
@@ -113,8 +112,70 @@ dataset$Outlet_Size <- NULL
 dataset$Outlet_Location_Type <- NULL
 
 #One Hot Encoding
+setDT(dataset)
 ohe = dummyVars("~.", data = dataset[,-c("Item_Identifier", "Outlet_Establishment_Year", "Item_Type")], fullRank = T) 
 ohe_df = data.table(predict(ohe, dataset[,-c("Item_Identifier", "Outlet_Establishment_Year", "Item_Type")])) 
 dataset = cbind(dataset[,"Item_Identifier"], ohe_df)
 
+#Preprocessing data
+dataset$Item_Visibility <- log(dataset$Item_Visibility + 1)
+dataset$price_per_unit_wt <- log(dataset$price_per_unit_wt + 1)
+
+num_vars = which(sapply(dataset, is.numeric))
+num_vars_names = names(num_vars)
+dataset_numeric = dataset[, setdiff(num_vars_names, dataset$Item_Outlet_Sales), with = F]
+prep_num = preProcess(dataset_numeric, method = c("center", "scale"))
+dataset_numeric_norm = predict(prep_num, dataset_numeric)
+dataset[, setdiff(num_vars_names, dataset$Item_Outlet_Sales) := NULL]
+dataset = cbind(dataset, dataset_numeric_norm)
+
+#Splitting the combined dataset
+
+train = dataset[1:nrow(train)]
+test = dataset[(nrow(train) + 1):nrow(dataset)]
+test[,Item_Outlet_Sales := NULL]
+
+#Correlated Variables 
+cor_train = cor(train[, -c("Item_Identifier")])
+corrplot(cor_train, method = "pie", type = "lower", tl.cex = 0.9)
+
+#Model Building - Linear Regression
+linear_reg_mod = lm(Item_Outlet_Sales ~ ., data = train[, -c("Item_Identifier")])
+submission$Item_Outlet_Sales = predict(linear_reg_mod, test[, -c("Item_Identifier")])
+write.csv(submission, "Linear_Reg_submit.csv", row.names = F)
+
+#Model Building - Regularized Linear Regression
+set.seed(1235)
+my_control = trainControl(method = "cv", number = 5)
+Grid = expand.grid(alpha = 1, lambda = seq(0.001, 0.1, by = 0.0002))
+lasso_linear_reg_mod = train(x = train[, -c("Item_Identifier", "Item_Outlet_Sales")], y = train$Item_Outlet_Sales, method='glmnet', trControl=my_control, tuneGrid=Grid)
+
+
+#Model Building - Random Forrest
+set.seed(1237)
+my_control = trainControl(method = "cv", number=5)
+tgrid = expand.grid(
+  .mtry = c(3:10),
+  .splitrule = "variance", 
+  .min.node.size = c(10, 15, 20)
+)
+rf_mod = train(x = train[, -c("Item_Identifier", "Item_Outlet_Sales")],
+               y = train$Item_Outlet_Sales, 
+               method = 'ranger',
+               trControl = my_control,
+               tuneGrid = tgrid,
+               num.trees = 400,
+               importance = "permutation"
+               )
+
+#Model Building - XGBoost
+param_list = list(objective = "reg:linear", eta=0.01, gamma = 1, max_depth=6, subsample=0.8, colsample_bytree=0.5)
+dtrain = xgb.DMatrix(data = as.matrix(train[,-c("Item_Identifier", "Item_Outlet_Sales")]), label= train$Item_Outlet_Sales)
+dtest = xgb.DMatrix(data = as.matrix(test[,-c("Item_Identifier")]))               
+
+set.seed(112)
+xgbcv = xgb.cv(params = param_list, data = dtrain, nrounds = 1000, nfold = 5, print_every_n = 10, early_stopping_rounds = 30, maximize = F)
+xgb_model = xgb.train(data = dtrain, params = param_list, nrounds = 430)
+var_imp = xgb.importance(feature_names = setdiff(names(train), c("Item_Identifier", "Item_Outlet_Sales")), model = xgb_model) 
+xgb.plot.importance(var_imp)
 
